1e77eff0db5f52a47ef54d6964f22f4d35ec1862
Luo Wei <lw@rock-chips.com>
2023-10-16 11:26:06 +0800

    rockchip: board: add serdes power init
    Signed-off-by: Luo Wei <lw@rock-chips.com>
    Change-Id: I9d1631ab54d07e123d078c55d0bc7bb183392c73

--- arch/arm/mach-rockchip/board.c
+++ arch/arm/mach-rockchip/board.c
@@ -53,7 +53,10 @@
 #ifdef CONFIG_ROCKCHIP_EINK_DISPLAY
 #include <rk_eink.h>
 #endif
-
+#ifdef CONFIG_SERDES_DISPLAY
+#include <serdes-display-core.h>
+#include <serdes-display-gpio.h>
+#endif
 DECLARE_GLOBAL_DATA_PTR;
 
 __weak int rk_board_late_init(void)
@@ -525,7 +528,9 @@ int board_init(void)
 	if (ab_decrease_tries())
 		printf("Decrease ab tries count fail!\n");
 #endif
-
+#ifdef CONFIG_SERDES_DISPLAY
+	serdes_power_init();
+#endif
 	return rk_board_init();
 }
 
